f186a8b707ef7f0f54dc5913904ab5ae6cf51b57
Alexander Demin <avdemin9@yandex.ru>
2019-12-03 23:33:26 +0300

    Сверстана страница main

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './pages/index.css';
+import './images/favicon.png';
+import './images/github_logo.png';
+import './images/facebook_logo.png';
+import './images/not-found.png';
+import './images/author_avatar.png';
